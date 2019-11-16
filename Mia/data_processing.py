149627414b6b60cd9fdf040e146be041941e95c0
miazhx <miazhx2008@gmail.com>
2019-11-15 22:49:40 -0500

    update comprehensive data processing/update data processing function/update elasticnet rmse 0.105

--- Mia/data_processing.py
+++ Mia/data_processing.py
@@ -1,23 +1,37 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[2]:
+# In[9]:
 
 
 import numpy as np
 import pandas as pd
 
+
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from math import sqrt
+from scipy import stats
+from scipy.stats import norm
+
+
+import matplotlib.pyplot as plt
+import matplotlib as matplotlib
+import seaborn as sns
+get_ipython().run_line_magic('matplotlib', 'inline')
+
 def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear_model = False):
 
 	# Make a copy so the original dataframe will not be altered.
     df_processed = df_raw.copy()
-
-	# Take the logarithm of the observed sales price.
-    df_processed.SalePrice = np.log(df_processed.SalePrice)
+    
     
 	# Remove outliers.
-    df_processed = df_processed[df_processed.GrLivArea < 4000]
+    outlier_list = [524, 1299, 463, 31, 534, 1433, 739, 1159, 108, 1231, 971, 1424]
+    df_processed = df_processed.drop(outlier_list)
 
+    
     ## Missing values
     
     # 259 LotFrontage  - replace missing value with 0 
@@ -36,6 +50,8 @@ def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear
     df_processed.BsmtExposure = df_processed.BsmtExposure.fillna('Nb')
     df_processed.BsmtFinType1 = df_processed.BsmtFinType1.fillna('Nb')
     df_processed.BsmtFinType2 = df_processed.BsmtFinType2.fillna('Nb')
+    df_processed.TotalBsmtSF = df_processed.TotalBsmtSF.fillna(0)
+    
 
     # 690 FireplaceQu - replace with Nf
     df_processed.FireplaceQu = df_processed.FireplaceQu.fillna('Nf')
@@ -59,7 +75,7 @@ def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear
     # 1 Electrical
     df_processed = df_processed[pd.notnull(df_processed.Electrical)]
 
-    ## Combine columns and drop direct multicollinear columns 
+    ## Combine columns and drop multicollinear columns 
     
     # combine bathroom quanlitity 
     df_processed['BsmtBath'] = df_processed.BsmtFullBath + df_processed.BsmtHalfBath * 0.5
@@ -67,14 +83,24 @@ def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear
     df_processed = df_processed.drop(['BsmtFullBath', 'BsmtHalfBath','FullBath','HalfBath'], axis=1)
 
     # drop TotalBsmtSF - multicollinearaty
-    df_processed = df_processed.drop(['TotalBsmtSF'], axis=1)
+    #df_processed = df_processed.drop(['TotalBsmtSF'], axis=1)
 
     # drop GrLivArea - multicollinearaty
-    df_processed = df_processed.drop(['GrLivArea'], axis=1)
+    #df_processed = df_processed.drop(['GrLivArea'], axis=1)
 
-    # drop GarageArea - results is better than drop GarageCars, why??
+    # drop GarageArea - higher correlation than GarageACars, results are better as well
     df_processed = df_processed.drop(['GarageArea'], axis=1) 
     
+    
+	# Feature Transformation - take the logarithm of the features.
+    #Linear_Num_Cols = ['TotalBsmtSF', '1stFlrSF', '2ndFlrSF', 'GrLivArea', 'LotArea', 'GarageArea', 'TotRmsAbvGrd', 'TotalSF', 'BsmtFinSF1']
+    df_processed.SalePrice = np.log(df_processed.SalePrice)
+    df_processed.GrLivArea = np.log(df_processed.GrLivArea)
+    df_processed.TotalBsmtSF = np.log(df_processed.TotalBsmtSF+1)
+#     df_processed.LotArea = np.log(df_processed.LotArea) -- performance decreases
+#     df_processed.GarageArea = np.log(df_processed.GarageArea)
+
+
 
 	# Categorical Features Processsing
 
@@ -87,7 +113,7 @@ def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear
     #Get Dummies 
     df_processed = pd.get_dummies(df_processed, columns=df_processed.select_dtypes(include=['object']).columns, drop_first=True)
 
-# #---Multiply features:
+# #---Multiply features: 
 #     df_processed["add_OverallGrade"] = df_processed["OverallQual"] * df_processed["OverallCond"]
 #     df_processed["add_GarageGrade"] = df_processed["GarageQual"] * df_processed["GarageCond"]
 #     df_processed["add_ExterGrade"] = df_processed["ExterQual"] * df_processed["ExterCond"]
@@ -101,4 +127,65 @@ def data_process(df_raw,remove_outlier = False,remove_hard_to_fit = False,linear
 
 
     return df_processed
+
+
+# In[16]:
+
+
+df_raw = pd.read_csv('train.csv',index_col=0)
+
+df_regression = data_process(df_raw)
+
+x_m = df_regression.drop(["SalePrice"], axis=1)
+y_m = df_regression.loc[:,'SalePrice']
+X_train, X_test, y_train, y_test = train_test_split(x_m, y_m, test_size=0.2, random_state=42)
+
+ols = LinearRegression()
+ols.fit(X_train, y_train)
+print("R^2 for train set: %f" %ols.score(X_train, y_train))
+
+print('-'*50)
+
+print("R^2 for test  set: %f" %ols.score(X_test, y_test))
+
+print('-'*50)
+rms = sqrt(mean_squared_error(y_test, ols.predict(X_test)))
+print("RMSE for test  set: %f" %rms)
+
+
+# In[12]:
+
+
+# test the assumptions 
+
+error = ols.predict(X_test) - y_test
+print('The mean of the errors is %.4f' %np.mean(error))
+print('The standard deviation of the errors is %.4f' % np.std(error))
+print("Skewness: %f" % error.skew())
+print("Kurtosis: %f" % error.kurt())
+sns.distplot(error, fit=norm);
+fig = plt.figure()
+res = stats.probplot(error, plot=plt)
+
+
+# High Kurtosis means there are outliers for the model which are hard to fit in the model 
+
+# In[4]:
+
+
+# plt.figure(figsize=(9, 6))
+# plt.xlim(-0.4, 0.4)
+# plt.ylim(0, 4.3)
+# plt.hist(error, color='blue', bins=10, normed=True, alpha=0.15)
+# my_norm = stats.norm(np.mean(error), np.std(error)).pdf
+# label = 'All samples,\nmean: %.4f, std: %.4f' % (np.mean(error), np.std(error))
+# plt.plot(np.linspace(-0.4, 0.4), my_norm(np.linspace(-0.4, 0.4)), label=label)
+# plt.legend()
+# plt.show()
+
+
+# In[13]:
+
+
+error.sort_values(ascending=False)
 
